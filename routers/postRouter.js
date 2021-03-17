70d0f4a14064972ab5e9434262df2bde1a9e8006
Ronit Raj <ronitraj.tg@gmail.com>
2021-03-18 00:05:18 +0530

    Add endpoints to delete and bookmark posts

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -36,9 +36,31 @@ router.get("/", auth, async (req, res) => {
 
 router.get("/my", auth, async (req, res) => {
   try {
-    // const user = await User.findById(req.user);
     const user = await User.findById(req.user).populate("posts");
     res.json(user.posts);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
+
+router.get("/bookmark", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user).populate("bookmarks");
+    res.json(user.bookmarks);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
+
+router.get("/bookmark/:id", auth, async (req, res) => {
+  try {
+    const bookmarkedPost = await Post.findById(req.params.id);
+    const userById = await User.findById(req.user);
+    userById.bookmarks.push(bookmarkedPost);
+    await userById.save();
+    res.json(bookmarkedPost);
   } catch (err) {
     console.error(err);
     res.status(500).send();
@@ -49,6 +71,22 @@ router.get("/:id", auth, async (req, res) => {
   try {
     const Posts = await Post.findById(req.params.id);
     res.json(Posts);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
+
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    await Post.findByIdAndDelete(req.params.id);
+    const userFound = await User.findById(req.user);
+    var index = userFound.posts.indexOf(req.params.id);
+    if (index !== -1) await userFound.posts.splice(index, 1);
+    index = userFound.bookmarks.indexOf(req.params.id);
+    if (index !== -1) await userFound.bookmarks.splice(index, 1);
+    userFound.save();
+    res.json(userFound);
   } catch (err) {
     console.error(err);
     res.status(500).send();
