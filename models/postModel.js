23273971e8d4ed450c7f15fa76f5b02e68ee3170
Ronit Raj <ronitraj.tg@gmail.com>
2021-03-18 17:29:44 +0530

    Add timestamps to models

--- models/postModel.js
+++ models/postModel.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
 
-const postSchema = new mongoose.Schema({
-  heading: { type: String, required: true },
-  description: { type: String, required: true },
-  postedBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
+const postSchema = new mongoose.Schema(
+  {
+    heading: { type: String, required: true },
+    description: { type: String, required: true },
+    postedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+    allowedBranches: [{ type: String }],
+    allowedYears: [{ type: String }],
+    allowedCollege: { type: String },
   },
-  allowedBranches: [{ type: String }],
-  allowedYears: [{ type: String }],
-  allowedCollege: { type: String },
-});
+  { timestamps: true }
+);
 
 const Post = mongoose.model("post", postSchema);
 
--- models/userModel.js
+++ models/userModel.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  passwordHash: { type: String, required: true },
-  college: { type: String, required: true },
-  year: { type: Number, required: true },
-  branch: { type: String, required: true },
-  bookmarks: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
-  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
-  postsRemaining: { type: Number },
-});
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true },
+    passwordHash: { type: String, required: true },
+    college: { type: String, required: true },
+    year: { type: Number, required: true },
+    branch: { type: String, required: true },
+    bookmarks: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
+    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
+    postsRemaining: { type: Number },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("user", userSchema);
 
